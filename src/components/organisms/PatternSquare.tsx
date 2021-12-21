c695a15791d34437fcfca49b7892c2fa191f00ae
yamoto0628 <a18sb009@zr.osaka-cu.ac.jp>
2021-12-21 17:49:22 +0900

    input train_dataをフロントから設定できるように

--- src/components/organisms/PatternSquare.tsx
+++ src/components/organisms/PatternSquare.tsx
@@ -5,7 +5,7 @@ type Props = {
   s: Array<Array<number>>,
 }
 
-const PatternSquare: React.FC<Props> = (props) => {
+const PatternSquare: React.FC<Props> = (props: Props) => {
   var rows = props.s.map((row, i)=>{
     return <PatternRow key={i} pattern_row={row} />
   });
