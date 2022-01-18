4125c1ce1cd68a11ab0442ad9ab1a67252a9813d
yamoyamoto <a18sb009@zr.osaka-cu.ac.jp>
2022-01-18 16:45:03 +0900

    production setting

--- libs/axios.tsx
+++ libs/axios.tsx
@@ -4,7 +4,7 @@ Axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 const production = process.env.NODE_ENV === "production";
 
 const axios = Axios.create({
-  baseURL: production ? "http://localhost:8000" : "https://fp-sumilation.herokuapp.com/",
+  baseURL: production ? "https://fp-sumilation.herokuapp.com/" : "http://localhost:8000",
 });
 
 export default axios;
